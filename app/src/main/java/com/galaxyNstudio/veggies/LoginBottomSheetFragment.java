db294759c2ed1a840d813126ffa808a95f35c770
bhat.vinayak <bhatprashant1994@gmail.com>
2019-05-27 17:28:23 +0530

    redirecting user to opt_signup activity

--- app/src/main/java/com/galaxyNstudio/veggies/LoginBottomSheetFragment.java
+++ app/src/main/java/com/galaxyNstudio/veggies/LoginBottomSheetFragment.java
@@ -18,10 +18,16 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import api.RetrofitClient;
 import butterknife.BindView;
+import model.MobileExist;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class LoginBottomSheetFragment extends BottomSheetDialogFragment implements View.OnClickListener{
+public class LoginBottomSheetFragment extends BottomSheetDialogFragment implements View.OnClickListener {
 
     EditText loginMobileEditText;
 
@@ -37,32 +43,30 @@ public class LoginBottomSheetFragment extends BottomSheetDialogFragment implemen
         setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle);
 
 
-
     }
-
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view= inflater.inflate(R.layout.bottom_sheet, container, false);
+        View view = inflater.inflate(R.layout.bottom_sheet, container, false);
 
 
-        loginMobileEditText=(EditText)view.findViewById(R.id.login_mobile_number);
-        butterLoginButton=(Button)view.findViewById(R.id.butterLoginButton);
+        loginMobileEditText = (EditText) view.findViewById(R.id.login_mobile_number);
+        butterLoginButton = (Button) view.findViewById(R.id.butterLoginButton);
         butterLoginButton.setOnClickListener(this);
-        if(loginMobileEditText != null){
+        if (loginMobileEditText != null) {
 
             loginMobileEditText.requestFocus();
             loginMobileEditText.postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     // TODO Auto-generated method stub
-                    InputMethodManager keyboard = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    InputMethodManager keyboard = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
                     keyboard.showSoftInput(loginMobileEditText, 0);
                 }
-            },100);
+            }, 100);
             loginMobileEditText.addTextChangedListener(new TextWatcher() {
 
                 @Override
@@ -78,18 +82,16 @@ public class LoginBottomSheetFragment extends BottomSheetDialogFragment implemen
 
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    if(s.length() == 10) {
+                    if (s.length() == 10) {
                         butterLoginButton.setEnabled(true);
                         butterLoginButton.setBackgroundColor(Color.BLACK);
                         butterLoginButton.setText("CONTINUE");
                         butterLoginButton.setEnabled(true);
-                    }
-                    else if(s.length() > 10) {
+                    } else if (s.length() > 10) {
                         butterLoginButton.setEnabled(false);
                         butterLoginButton.setBackgroundColor(android.graphics.Color.parseColor("#BFBFBF"));
                         butterLoginButton.setText("INVALID MOBILE NUMBER");
-                    }
-                    else if(s.length() < 10){
+                    } else if (s.length() < 10) {
                         butterLoginButton.setEnabled(false);
                         butterLoginButton.setBackgroundColor(android.graphics.Color.parseColor("#BFBFBF"));
                         butterLoginButton.setText("PLEASE ENTER MOBILE NUMBER");
@@ -107,12 +109,61 @@ public class LoginBottomSheetFragment extends BottomSheetDialogFragment implemen
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.butterLoginButton:
-                Intent intent = new Intent(getActivity(), SignUpActivity.class);
-                intent.putExtra("phone",loginMobileEditText.getText().toString().trim());
-                startActivity(intent);
+
+                String mobile = loginMobileEditText.getText().toString().trim();
+
+                //TODO  check first whether user is already registered or not,
+                //TODO if User already registered then redirect to otp activity else redirect to signup activity
+                checkUserAlreadyPresent(mobile);
+
+
 
                 break;
 
         }
     }
+
+    public void checkUserAlreadyPresent(final String mobile) {
+
+        boolean flag=false;
+
+        Call<MobileExist> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .checkUserMobileExists(mobile);
+
+        call.enqueue(new Callback<MobileExist>() {
+            @Override
+            public void onResponse(Call<MobileExist> call, Response<MobileExist> response) {
+                if (response.code() == 201) {
+
+                    MobileExist res = response.body();
+                    if (res.getError().equals(Boolean.TRUE)) {
+                        // redirect to OTP screen
+                        Intent intent = new Intent(getActivity(), OtpActivity.class);
+                        intent.putExtra("phone", mobile);
+                        startActivity(intent);
+                    } else if (res.getError().equals(Boolean.FALSE)) {
+                        //new user creation so redirect to signup activity
+                        Intent intent = new Intent(getActivity(), SignUpActivity.class);
+                        intent.putExtra("phone", mobile);
+                        startActivity(intent);
+                    }
+                    Toast.makeText(getActivity(), res.getMessage(), Toast.LENGTH_LONG).show();
+
+                } else if (response.code() == 422) {
+                    Toast.makeText(getActivity(), "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MobileExist> call, Throwable t) {
+                Toast.makeText(getActivity(), "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
 }
+
+
---
+++ app/src/main/java/model/MobileExist.java
@@ -0,0 +1,28 @@
+package model;
+
+public class MobileExist {
+
+    private String error;
+    private String message;
+
+    public MobileExist(String error, String message) {
+        this.error = error;
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
